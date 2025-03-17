8749cda005fd038524e7e7254f72c7b4cdf62fde
privatepharmacy <kabirsabirba@gmail.com>
2025-03-17 19:25:16 +0000

    Create send_message.php

---
+++ send_message.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "info@privatepharmacyclinic.com"; // Change this to your actual email
+    $subject = "New Contact Form Message";
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error sending message.";
+    }
+}
+?>
